72748ab85f3aafb9b8930bdbc680c75c28561fde
kauanlisboadias <kauanlisboaasantos@gmail.com>
2023-06-15 12:28:49 -0300

    Balonismo versão 1.0

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,14 @@ public class Main {
 
         //. . .
         p1.nome = "Elliot Page";
+        p1.cpf = "191.566.456-12";
+        p1.termoCienciaRisco = true;
+        p1.rg = "666666666/77";
+        p1.rg =
+
+
+        //piloto
+         piloto p3 = new piloto();
+         piloto p4;
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,7 +1,13 @@
 public class Pessoa {
 
     // Atributos
-    String nome;
+   public String nome;
+   public String cpf; // 111.111.111-11
+    boolean termoCienciaRisco;
+   public String rg;
+   public String telefone;
+   public String telContEmerg;
+   public String endereco;
 
     // Métodos
     public void pesquisarEmpresa(){
